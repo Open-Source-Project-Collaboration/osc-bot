f27c5395ad9334c58d5ffa00298508f5c8b04d9f
DejaVu <60882185+Deja-Vu1@users.noreply.github.com>
2020-08-11 12:59:00 +0300

    Update bot.py
    change ":" to "#!"

--- bot.py
+++ bot.py
@@ -2,17 +2,17 @@ import discord
 from discord.ext import commands
 
 perfix = ">"
-bot = commands.Bot(command_prefix=perfix)
+bot = commands.Bot(command_prefix='Bot')
 
 
 @bot.listen()
 async def on_message(message):
-    if message.content.lower()[0] == "#!" :
+    if message.content.lower()[0:2] == "#!" :
         who = str(message.author)
         if "-" in message.content.lower():
             project = message.content.split("-")
             emoji = '\N{THUMBS UP SIGN}'
-            a = await message.channel.send('> Added project please voted\n_Project Name:_\n**' + project[0][1:len(project[0])] + "**\n_Description:_\n**" + project[1] + "**\n\nBy **" + who[0:len(who)-5] + "**")
+            a = await message.channel.send('> Added project please voted\n_Project Name:_\n**' + project[0][2:len(project[0])] + "**\n_Description:_\n**" + project[1] + "**\n\nBy **" + who[0:len(who)-5] + "**")
             await message.delete() 
             await a.add_reaction(emoji)
         else:
