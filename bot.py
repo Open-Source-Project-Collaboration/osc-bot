5a0e1dc2bb25e2d86b37a024a4562d29acdb0557
İlkan Seçkin <ilkanseckin@gmail.com>
2020-08-11 16:35:18 +0300

    Added everythings (1. save project names and message id, 2. print who remove reactions or who add reactions, 3. print error code)

--- bot.py
+++ bot.py
@@ -1,63 +1,63 @@
 import discord
 from discord.ext import commands
 
-perfix = ">"
-bot = commands.Bot(command_prefix='Bot')
+perfix = "!"
+bot = commands.Bot(command_prefix=perfix)
 
-@bot.listen()
-async def on_message(message):
+@bot.command()
+async def new_idea(ctx,name,description):
     projectlogs = open("projectlogs.txt","r")
     logs = projectlogs.read()
-    if message.content.lower()[0:2] == "#!" :
-        who = str(message.author)
-        if "-" in message.content.lower():
-            project = message.content.split("-")
-            emoji = '\N{THUMBS UP SIGN}'
-            if logs.find(project[0][2:len(project[0])]) > -1:
-                await message.channel.send("{0.author.mention} Please type another project name".format(message))
-            else:
-                projectlogs.close()
-                projectlogs = open("projectlogs.txt","a")
-                a = await message.channel.send('> Added project please voted\n_Project Name:_\n**' + project[0][2:len(project[0])] + "**\n_Description:_\n**" + project[1] + "**\n\nBy **" + who[0:len(who)-5] + "**")
-                await message.delete() 
-                await a.add_reaction(emoji)
-                projectlogs.write(str(a.id) + "|" + project[0][2:len(project[0])] + "|")
-        else:
-            await message.channel.send("{0.author.mention} Please add description (-this is description)".format(message))
+    who = str(ctx.message.author)
+    emoji = '\N{THUMBS UP SIGN}'
+    if str(ctx.message.channel) != "full-testing":
+        await ctx.message.channel.send("{0.author.mention} Please type on #full-testing".format(ctx.message))
+    elif logs.find(str(name)) != -1:
+        await ctx.message.channel.send("{0.author.mention} There is a project with this name".format(ctx.message))
+    elif name == "":
+        await ctx.message.channel.send("{0.author.mention} Please add Project Name (!new_idea 'this is Project Name' 'this is description')".format(ctx.message))
+    elif description == "":
+        await ctx.message.channel.send("{0.author.mention} Please add description (!new_idea 'this is Project Name' 'this is description')".format(ctx.message))
+    elif str(ctx.message.channel) == "full-testing" and logs.find(str(name)) == -1:
+         projectlogs.close()
+         projectlogs = open("projectlogs.txt","a")
+         a = await ctx.message.channel.send('> Added project please voted\n_Project Name:_\n**' + name + "**\n_Description:_\n**" + description + "**\n\nBy **" + who[0:len(who)-5] + "**")
+         await ctx.message.delete() 
+         await a.add_reaction(emoji)
+         projectlogs.write(str(a.id) + "|" + name + "|")
     projectlogs.close()
 
-    
 @bot.event
 async def on_raw_reaction_add(message):
     projectlogs = open("projectlogs.txt","r")
     logs = projectlogs.read().split("|")
-    #print(message.user_id)
-    #print(message.member.name)
-    #print(message.emoji.name)
     wichproject = ""
-    #print(message)
     if  message.emoji.name == '👍' :
         for i in range(len(logs)):  #This for while is search projects by message id
-            #print(str(logs[i]) == str(message.message_id))
-            #print(str(logs[i]))
-            if logs[i] == message.message_id:
+            if logs[i] == str(message.message_id):
                  wichproject = logs[i+1]
-                 print(message.member.name + " is voted for " + wichproject)
+                 print(str(message.user_id) + " is voted for " + wichproject)
                  break
     projectlogs.close()
 
-    
+@bot.event
 async def on_raw_reaction_remove(message):
     projectlogs = open("projectlogs.txt","r")
     logs = projectlogs.read().split("|")
     wichproject = ""
-    #print(message)
-    if  message.emoji.name == '👎':
-        for i in range(len(logs)):  #This for while is search projects by message id
-            #print(str(logs[i]) == str(message.message_id))
-            if str(logs[i]) == str(message.message_id):
+    if  message.emoji.name == '👍' : 
+       for i in range(len(logs)):  #This for while is search projects by message id
+            if logs[i] == str(message.message_id):
                 wichproject = logs[i+1]
-                print(message.member.name + " is removed vote for " + wichproject)
+                print(str(message.user_id) + " is removed vote for " + str(wichproject))
                 break
     projectlogs.close()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
 bot.run('Token')
