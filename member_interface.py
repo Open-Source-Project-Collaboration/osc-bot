7b73d6aed18bd59986865974b23d6a25ae047ca6
Brikaa <40431124+Brikaa@users.noreply.github.com>
2020-08-13 21:31:47 +0200

    PEP8 and code improvements
    Fixed some errors and reformated some lines

---
+++ member_interface.py
@@ -0,0 +1,21 @@
+config = {
+    'idea-channel': '742718894690795550'
+}
+
+
+# Setup function
+def setup_member_interface(bot):
+    # Show currently used idea channel
+    @bot.command()
+    async def idea_channel(ctx):
+        chanid = config['idea-channel']
+        await ctx.send(f'Current idea channel is <#{chanid}>')
+
+    # Proposes a new idea to idea channel
+    @bot.command()
+    async def new_idea(ctx, lang, idea):
+        chanid = int(config['idea-channel'])
+        chan = bot.get_channel(chanid)
+
+        msg = await chan.send(f'{ctx.author.mention} proposed an idea:\n\n> {idea}\n\n In `{lang}`')
+        await msg.add_reaction('👍')
