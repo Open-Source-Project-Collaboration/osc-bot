10b8fc959452cc5658be51e7a603d92be00d27fc
Omar Brikaa <adel.brikaa@hotmail.com>
2020-08-19 13:43:31 +0200

    - Bot checks for unfinished ideas on boot asynchronously by adding a restart emoji to each of the unfinished idea messages. - If an idea message has a restart emoji, call the wait_for_votes function - Users/admins can only vote with thumbs up

--- src/main.py
+++ src/main.py
@@ -29,49 +29,5 @@ setup_admin_interface(bot)
 Config.set_init('idea-channel', '744885478188384287')
 Config.set_init('overview-channel', '744885556613480509')
 
-
-# Watch for reaction add
-@bot.event
-async def on_raw_reaction_add(reaction):
-    # Get the project role
-    idea_id = int(Config.get('idea-channel'))
-    chan = bot.get_channel(idea_id)
-    message = await chan.fetch_message(reaction.message_id)
-    if reaction.channel_id == idea_id and message.author.bot and message.embeds:
-        embed = message.embeds[0]
-        guild = bot.get_guild(reaction.guild_id)
-        member = guild.get_member(reaction.user_id)
-        role = discord.utils.get(guild.roles, name=embed.title)
-
-        # Add user to role
-        await member.add_roles(role)
-
-
-# Watch for reaction remove
-@bot.event
-async def on_raw_reaction_remove(reaction):
-    # Get the project role
-    chan = bot.get_channel(reaction.channel_id)
-    message = await chan.fetch_message(reaction.message_id)
-    idea_id = int(Config.get('idea-channel'))
-    if reaction.channel_id == idea_id and message.author.bot:
-        embed = message.embeds[0]
-        guild = bot.get_guild(reaction.guild_id)
-        member = guild.get_member(reaction.user_id)
-        role = discord.utils.get(guild.roles, name=embed.title)
-        # Remove user from role
-
-        if member == message.mentions[0]:  # If the reaction remover is the owner of the idea
-            if len(role.members) > 1:  # If there are members in the current role
-                new_content = message.content.replace(message.mentions[0].mention, role.members[1].mention)
-                await message.edit(content=new_content)
-                # Replace the owner with the second member in the role
-
-            else:
-                await message.edit(content=message.content.replace(message.mentions[1].mention, "No owner"))
-
-        await member.remove_roles(role)
-
-
 # Run bot
 bot.run(environ.get('DISCORD_TOKEN'))
--- src/member_interface.py
+++ src/member_interface.py
@@ -5,6 +5,7 @@ import discord.ext.commands.errors
 TIME_TO_WAIT = 10
 CHECK_MARK_EMOJI = '\U0001F973'
 REQ_VOTES = 0
+RESTART_EMOJI = '\U0001F504'
 
 
 # Setup function
@@ -19,7 +20,7 @@ def setup_member_interface(bot):
 
     # Proposes a new idea to idea channel
     @bot.command(brief="Adds a new idea to the ideas channel")
-    async def new_idea(ctx, lang='', idea_name=''):
+    async def new_idea(ctx, lang='', idea_name='', idea_explanation='N/A'):
 
         # Check fields
         if not lang or not idea_name:
@@ -35,6 +36,11 @@ def setup_member_interface(bot):
 
         # Generate a name from idea
         gen_name = '-'.join(idea_name.split(' '))
+
+        role = discord.utils.get(ctx.guild.roles, name=gen_name)
+        if role:
+            return await ctx.send(ctx.author.mention + ", this idea name already exists.")
+
         try:
             # Create a role for it
             role = await ctx.guild.create_role(name=gen_name)
@@ -44,6 +50,7 @@ def setup_member_interface(bot):
 
             # Notify with embed
             embed = discord.Embed(title=gen_name, color=0x00ff00)
+            embed.add_field(name="Idea Explanation", value=idea_explanation)
             embed.add_field(name='Programming Language', value=lang, inline=False)
             msg = await chan.send(f'{ctx.author.mention} proposed an idea:', embed=embed)
             await msg.add_reaction('👍')
@@ -111,22 +118,76 @@ def setup_member_interface(bot):
 
         idea_id = int(Config.get('idea-channel'))
         idea_channel = bot.get_channel(idea_id)
-        overview_id = int(Config.get('overview-channel'))
-        overview_channel = bot.get_channel(overview_id)
         messages = await idea_channel.history().flatten()
         for message in messages:  # Loop through the messages in the ideas channel
-            if message.embeds:  # If the message is an idea message containing Embed
-                print("Found an unfinished idea")
-                idea_name = message.embeds[0].title  # The idea name is the title of the Embed
-                # Notify users
-                await overview_channel.send(f'There has been a problem processing `{idea_name}`\n' +
-                                            "The voting period was restarted but your votes are safe.")
-
-                for role in message.guild.roles:  # Loop through each role
-                    if role.name == idea_name:  # If there is a role for the idea
-                        return await wait_for_votes(message, role)
-
-                role = await message.guild.create_role(name=idea_name)  # This is reached when the role has been deleted
-                await wait_for_votes(message, role)
+            if message.embeds:  # If the message is an idea message containing Embed, add the restart emoji
+                print("Found an unfinished idea!")
+                await message.add_reaction(RESTART_EMOJI)
 
         print("No unfinished ideas since last boot")
+
+    # Watch for reaction add
+    @bot.event
+    async def on_raw_reaction_add(reaction):
+        idea_id = int(Config.get('idea-channel'))
+        idea_channel = bot.get_channel(idea_id)
+        overview_id = int(Config.get('overview-channel'))
+        overview_channel = bot.get_channel(overview_id)
+
+        if reaction.channel_id != idea_id:  # Makes sure the reaction added is in the ideas channel
+            return
+
+        message = await idea_channel.fetch_message(reaction.message_id)
+        if message.author.bot and message.embeds:  # If the message reacted to is by the bot and contains an embed
+            # ie: it is an idea message
+            embed = message.embeds[0]
+            guild = bot.get_guild(reaction.guild_id)
+
+            if reaction.emoji.name == '\N{THUMBS UP SIGN}':  # If it is a thumbs up emoji, add the idea role
+                member = guild.get_member(reaction.user_id)
+                role = discord.utils.get(guild.roles, name=embed.title)
+
+                # Add user to role
+                await member.add_roles(role)
+
+            elif reaction.emoji.name == RESTART_EMOJI and reaction.member.bot:
+                # if it is a restart emoji put by the bot, restart the voting period
+                idea_name = embed.title
+                await overview_channel.send(f'An error occurred while processing the `{idea_name}` idea\n' +
+                                            "The voting period has been restarted but your votes are safe.")
+                role = discord.utils.get(message.guild.roles, name=idea_name)
+                # We remove the reaction in case the voting period gets restarted again
+                await message.remove_reaction(reaction.emoji, reaction.member)
+                if role:
+                    await wait_for_votes(message, role)
+                else:  # The role was deleted
+                    role = await message.guild.create_role(name=idea_name)
+                    await wait_for_votes(message, role)
+
+            else:  # If it is another emoji, remove the reaction
+                await message.remove_reaction(reaction.emoji, reaction.member)
+
+    # Watch for reaction remove
+    @bot.event
+    async def on_raw_reaction_remove(reaction):
+        # Get the project role
+        chan = bot.get_channel(reaction.channel_id)
+        message = await chan.fetch_message(reaction.message_id)
+        idea_id = int(Config.get('idea-channel'))
+        if reaction.channel_id == idea_id and message.author.bot and reaction.emoji.name == '\N{THUMBS UP SIGN}':
+            embed = message.embeds[0]
+            guild = bot.get_guild(reaction.guild_id)
+            member = guild.get_member(reaction.user_id)
+            role = discord.utils.get(guild.roles, name=embed.title)
+            # Remove user from role
+
+            if member == message.mentions[0]:  # If the reaction remover is the owner of the idea
+                if len(role.members) > 1:  # If there are members in the current role
+                    new_content = message.content.replace(message.mentions[0].mention, role.members[1].mention)
+                    await message.edit(content=new_content)
+                    # Replace the owner with the second member in the role
+
+                else:
+                    await message.edit(content=message.content.replace(message.mentions[1].mention, "No owner"))
+
+            await member.remove_roles(role)
