c0df273fa6f9525b50687c3df154426888bccb75
Omar Brikaa <brikaaomar@gmail.com>
2020-09-19 13:53:20 +0200

    Added the reddit_post function

--- src/reddit/reddit_interface.py
+++ src/reddit/reddit_interface.py
@@ -1,3 +1,14 @@
-def setup_reddit_interface(bot):  # Bot commands related to the reddit implementation go here,
+import discord.ext.commands
+from team import Team
+
+
+def setup_reddit_interface(bot: discord.ext.commands.Bot):  # Bot commands related to the reddit implementation go here,
     # it is preferable to add functions to another file
-    pass
+    @bot.command(brief="Lets the bot make a reddit post about the team")
+    async def reddit_post(ctx: discord.ext.commands.Context):
+        category: discord.CategoryChannel = ctx.channel.category  # Gets the category in which the command was used
+        if not category:  # If the command was used in a channel that does not belong to a category
+            return await ctx.send("Please use this command in a team channel")
+        team: Team = Team.get(category_id=category.id)  # Gets the team according to the category id
+        if not team:  # If not team was found for this category
+            return await ctx.send("Please use this command in a team channel")
