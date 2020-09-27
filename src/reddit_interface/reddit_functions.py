87bf62c08b549351a4c53f84c0901d09f91fe3c5
Omar Brikaa <brikaaomar@gmail.com>
2020-09-27 16:58:34 +0200

    Completed the show_post_preview function

--- src/reddit_interface/reddit_functions.py
+++ src/reddit_interface/reddit_functions.py
@@ -35,14 +35,12 @@ async def get_post_input(bot: discord.ext.commands.Bot, ctx, templates_list, emb
 
                                         "Type `r: create` to create your own template", embed=embed)
 
-    def check(m: discord.Message):
-        return m.author == ctx.author and m.channel == ctx.channel and 'r:' in m.content.lower()
-
-    message = await bot.wait_for('message', check=check, timeout=300)
+    message = await wait_for_reddit_message(bot, ctx)
     if not message:
-        return message
+        return
+    response = message.content[2:].lstrip()
 
-    if "another" in message.content.lower():
+    if response.lower() == "another":
         if len(templates_list) < 2:
             await ctx.send("Other templates are not available at the moment.")
             return await get_post_input(bot, ctx, templates_list, embed, *formatting)
@@ -53,28 +51,57 @@ async def get_post_input(bot: discord.ext.commands.Bot, ctx, templates_list, emb
         embed = discord.Embed(title=embed.title, description=new)
         return await get_post_input(bot, ctx, templates_list, embed, *formatting)
 
-    elif "create" in message.content.lower():
+    elif response.lower() == "create":
         return await get_new_template(bot, ctx)
 
     else:
-        information = message.content[2:].lstrip()
-        return embed.description.replace("...", information)
+        return embed.description.replace("...", response)
 
 
-async def show_post_preview(bot: discord.ext.commands.Bot, ctx: discord.ext.commands.Context, title, body):
+async def show_post_preview(bot: discord.ext.commands.Bot, ctx: discord.ext.commands.Context, title, body,
+                            subreddit=None):
     await ctx.send("Please type `r: [language name]`, where [language name] is "
                    "the programming language that is used in the project")
 
     programming_language_message = await wait_for_reddit_message(bot, ctx)
+    if not programming_language_message:
+        return
+
     programming_language = programming_language_message.content[2:].lstrip().lower()
+
     # Tries to find a subreddit in the database that corresponds to the programming language
     language_subreddits = Language.get_all_subreddits(programming_language) or Language.get_all_subreddits('general')
-    language_subreddit = 'testosc' if not language_subreddits else random.choice(language_subreddits).subreddit
+    while True:
+        language_subreddit = 'testosc' if not language_subreddits else random.choice(language_subreddits).subreddit
+        if language_subreddit != subreddit:
+            break
 
     embed = discord.Embed(title=title, description=body)
-    message = await ctx.send("Here is how your post will look like on reddit.\n"
-                             f"The submission will be made in r/{language_subreddit}\n"
-                             "Use `r: confirm` to confirm\n"
-                             "Use `r: another` to change the subreddit\n"
-                             "Use `r: cancel` to cancel the submission", embed=embed)
+    content = "Here is how your post will look like on reddit.\n" \
+              f"The submission will be made in r/{language_subreddit}\n" \
+              "Use `r: confirm` to confirm\n" \
+              "Use `r: cancel` to cancel the submission"
+    if len(language_subreddits) > 1:
+        content += "Use `r: another` to change the subreddit\n"
+
+    await ctx.send(content, embed=embed)
+    response_message = await wait_for_reddit_message(bot, ctx)
+    if not response_message:
+        return
+
+    response = response_message.content[2:].lstrip().lower()
+
+    if response == "another" and len(language_subreddits) > 1:
+        return await show_post_preview(bot, ctx, title, body, subreddit=language_subreddit)
+    elif response == "another":
+        await ctx.send("You can't change the subreddit for this case")
+        return await show_post_preview(bot, ctx, title, body)
+    elif response == "confirm":
+        return title, body, language_subreddit
+    elif response == "cancel":
+        return
+    else:
+        await ctx.send("Invalid option.")
+        return await show_post_preview(bot, ctx, title, body)
+
     pass
--- src/reddit_interface/reddit_interface.py
+++ src/reddit_interface/reddit_interface.py
@@ -53,5 +53,9 @@ def setup_reddit_interface(bot: discord.ext.commands.Bot):  # Bot commands and e
         body += "\n\n" + random.choice(footers).format(discord_user, discord_bot)
 
         # Shows the preview of the post to be sent to the reddit channel
-        await show_post_preview(bot, ctx, title, body)
+        post_data = await show_post_preview(bot, ctx, title, body)
+        if not post_data:
+            return await ctx.send(ctx.author.mention + ", your post has been cancelled.")
+        title, body, subreddit = post_data
+        print(title, body, subreddit)
         pass
